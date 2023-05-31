6912f0d10e8c484023ca8183ac206b992e999ec5
wexy021 <wexy021@gmail.com>
2023-05-31 18:50:00 -0500

    login wip

--- src/components/pages/login.jsx
+++ src/components/pages/login.jsx
@@ -1,10 +1,36 @@
 import React from "react";
+import styled from 'styled-components';
+
+const loginbox = styled.div`
+  border: 2px solid black;
+  background-color: white;
+  box-shadow: 5px 5px black;
+  margin: 10px;
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  background-image: linear-gradient(
+      rgba(0, 0, 0, 0.05) 0.1em,
+      transparent 0.1em
+    ),
+    linear-gradient(90deg, rgba(0, 0, 0, 0.05) 0.1em, transparent 0.1em);
+  background-size: 0.7em 0.7em;
+`;
+//or should we just route people to signup page?
+if (!username || !password) {
+	alert('Please enter correct username or password!')
+}
+
+
 
 function LogIn() {
 	return (
 		<>
-			<UserName />
-			<Password />
+			<div className="loginbox">
+				<input type="text"/>
+				<input type="password" />
+			</div>
 		</>
 	)
 }
